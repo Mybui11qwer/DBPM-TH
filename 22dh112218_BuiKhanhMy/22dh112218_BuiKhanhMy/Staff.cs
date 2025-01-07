c33c31b758885e5fd80f20a9ff621265330a9557
HAPM03-PC14\SV <mybui11qwer@gmail.com>
2025-01-07 17:27:59 +0700

    7/1

---
+++ 22dh112218_BuiKhanhMy/22dh112218_BuiKhanhMy/Staff.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22dh112218_BuiKhanhMy
+{
+    public class Staff
+    {
+        private int MsNV;
+        private string TenNV;
+        private DateTime NgaySinh;
+        private int SoGioLam;
+        private double LuongCB; //Lương cơ bản cho 1 ngày làm
+        private double LuongThuong;
+        private string BangCap;
+        private string SoNgayNghi;
+
+        public Staff() { }
+        public Staff(int msNV, string tenNV, DateTime ngaysinh, int soGioLam, string bangcap)
+        {
+            this.MsNV = msNV;
+            this.TenNV = tenNV;
+            this.SoGioLam = soGioLam;
+            this.NgaySinh = ngaysinh;
+            this.BangCap = bangcap;
+        }
+
+        public double TinhNgayCong()
+        {
+            //Mỗi ngày làm 8 tiếng.
+            return SoGioLam / 8;
+        }
+        public double TinhLuong()
+        {
+            return TinhNgayCong() * LuongCB + LuongThuong;
+        }
+    }
+}
